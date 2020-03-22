15194dea8d081e8c4fc2a4d7bfaba6db091e6d00
Max Mitrani <mjmitrani168@gmail.com>
2020-03-22 15:16:32 -0600

    Add deck1,deck2,player1,player2 to runner

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -5,3 +5,34 @@ require './lib/turn'
 
 class Start
 end
+
+
+deck1 = Deck.new([])
+deck2 = Deck.new([])
+player1 = Player.new("Megan", deck1)
+player2 = Player.new("Aurora", deck2)
+card_holder =
+
+if
+  card_count = 1
+  heart_count = 0
+  diamond_count = 0
+  club_count = 0
+  spade_count = 0
+  card_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+
+
+
+
+    if
+
+    elsif hear_count = 14
+    card_ranks_count = 0
+    card.card_count = Card.new(:heart, card_rank[card_ranks_count], card_ranks_count + 2)
+
+
+
+
+
+card_count += 1
