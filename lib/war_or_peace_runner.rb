c67ddaf2456f6efcf7b3d84da532437972322eaa
Max Mitrani <mjmitrani168@gmail.com>
2020-03-22 17:18:38 -0600

    add deck building framework

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -8,24 +8,34 @@ p "Welcome to War! (or Peace) This game will be played with 52 cards.
 The players today are Megan and Aurora."
 p "Type 'GO' to start the game!"
 start_game = gets.chomp
-#intro
 
-class Start
+card_holder = []
 
+card_value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+
+card_value.each do |value|
+  card_count = 1
+  card_holder << Card.new(:heart, value, card_count + 1)
+
+
+  # card = Card.new(:diamond, value, card_count)
+  # card = Card.new(:club, value, card_count)
+  # card = Card.new(:spade, value, card_count)
+  card_count += 1
 end
-
+binding.pry
 
 deck1 = Deck.new([])
 deck2 = Deck.new([])
 player1 = Player.new("Megan", deck1)
 player2 = Player.new("Aurora", deck2)
-card_holder = []
-card_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-card_suits = [:heart, :diamond, :club, :spade]
-card_ranks.each do |rank|
-  card_count = 1
-  card + card_count.to_s = Card.new(:heart, card_ranks, card_count)
-end
+
+
+
+
+
+
 
 
 
