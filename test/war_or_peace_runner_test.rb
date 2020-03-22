109bb27025c519b663d93a07f07b57776fdfccae
Max Mitrani <mjmitrani168@gmail.com>
2020-03-22 16:13:49 -0600

    Add test for card_holder

--- test/war_or_peace_runner_test.rb
+++ test/war_or_peace_runner_test.rb
@@ -4,3 +4,17 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+
+class TurnTest < Minitest::Test
+
+  def test_52_cards
+    deck1 = Deck.new([])
+    deck2 = Deck.new([])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    card_holder = []
+    card_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    card_suits = [:heart, :diamond, :club, :spade]
+
+    assert_equal card_holder = []
+  end 
