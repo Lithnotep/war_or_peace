cbdbef0234d1a957a9684f144e336fcc8567c75c
Max Mitrani <mjmitrani168@gmail.com>
2020-03-22 16:23:08 -0600

    Add intro for game puts

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -4,7 +4,10 @@ require './lib/player'
 require './lib/turn'
 require 'pry'
 
-
+p "Welcome to War! (or Peace) This game will be played with 52 cards.
+The players today are Megan and Aurora."
+p "Type 'GO' to start the game!"
+start_game = gets.chomp
 #intro
 
 class Start
@@ -16,10 +19,18 @@ deck1 = Deck.new([])
 deck2 = Deck.new([])
 player1 = Player.new("Megan", deck1)
 player2 = Player.new("Aurora", deck2)
-
-
 card_holder = []
 card_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+card_suits = [:heart, :diamond, :club, :spade]
+card_ranks.each do |rank|
+  card_count = 1
+  card + card_count.to_s = Card.new(:heart, card_ranks, card_count)
+end
+
+
+
+
+
 
 
 
@@ -27,4 +38,3 @@ card_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "Ki
 
 
 binding.pry
-end
