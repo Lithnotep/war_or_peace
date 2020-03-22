42369f900a12a6ea333e74a04c5eca7ed94cdc0e
Max Mitrani <mjmitrani168@gmail.com>
2020-03-22 16:03:36 -0600

    Add clean up of bad code

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -8,7 +8,7 @@ require 'pry'
 #intro
 
 class Start
-  def initialize()
+
 end
 
 
@@ -19,18 +19,12 @@ player2 = Player.new("Aurora", deck2)
 
 
 card_holder = []
+card_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 
 
-if card_holder.count < 52
-  card_count = 1
 
-  card_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-  14.times do |heart|
-    new_card = []
-    card_ranks_count = 0
-    new_card << card + card_count.to_s = Card.new(:heart, card_rank[card_ranks_count], card_ranks_count + 2)
-    card_holder << new_card
-    card_count += 1
-  end
+
+
+
 binding.pry
 end
