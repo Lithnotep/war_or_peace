57b3e39adde6219e32e1d69dc72ac395ddfd92cf
Max Mitrani <mjmitrani168@gmail.com>
2020-03-22 16:02:06 -0600

    Add attempt at making deck of cards

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -2,8 +2,13 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require 'pry'
+
+
+#intro
 
 class Start
+  def initialize()
 end
 
 
@@ -11,28 +16,21 @@ deck1 = Deck.new([])
 deck2 = Deck.new([])
 player1 = Player.new("Megan", deck1)
 player2 = Player.new("Aurora", deck2)
-card_holder =
 
-if
+
+card_holder = []
+
+
+if card_holder.count < 52
   card_count = 1
-  heart_count = 0
-  diamond_count = 0
-  club_count = 0
-  spade_count = 0
+
   card_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-
-
-
-
-
-    if
-
-    elsif hear_count = 14
+  14.times do |heart|
+    new_card = []
     card_ranks_count = 0
-    card.card_count = Card.new(:heart, card_rank[card_ranks_count], card_ranks_count + 2)
-
-
-
-
-
-card_count += 1
+    new_card << card + card_count.to_s = Card.new(:heart, card_rank[card_ranks_count], card_ranks_count + 2)
+    card_holder << new_card
+    card_count += 1
+  end
+binding.pry
+end
