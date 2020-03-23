e15be71910f26de20f9ceae9b0a40871dada37eb
Max Mitrani <mjmitrani168@gmail.com>
2020-03-22 18:31:11 -0600

    Add Shuffle to deck creaction

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -17,34 +17,16 @@ player2 = Player.new("Aurora", deck2)
 turn = Turn.new(player1, player2)
 card_holder = []
 card_value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+card_count = 0
 card_value.each do |value|
-  card_count = 1
+  card_count += 1
   card_holder << Card.new(:heart, value, card_count + 1)
   card_holder << Card.new(:diamond, value, card_count + 1)
   card_holder << Card.new(:club, value, card_count + 1)
   card_holder << Card.new(:spade, value, card_count + 1)
-  card_count += 1
 end
-player1.deck.cards << card_holder.shift(25)
+card_holder.shuffle!(random: Random.new(1))
+player1.deck.cards << card_holder.shift(26)
+player1.deck.cards.flatten!
 player2.deck.cards << card_holder
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-binding.pry
+player2.deck.cards.flatten!
