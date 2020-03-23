9fd5fd50e58f242884f4c280c9343d09209ec322
Max Mitrani <mjmitrani168@gmail.com>
2020-03-22 18:13:41 -0600

    Add deck1, deck2 creation and tests

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -9,27 +9,27 @@ The players today are Megan and Aurora."
 p "Type 'GO' to start the game!"
 start_game = gets.chomp
 
-card_holder = []
-
-card_value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-
-
-card_value.each do |value|
-  card_count = 1
-  card_holder << Card.new(:heart, value, card_count + 1)
-
-
-  # card = Card.new(:diamond, value, card_count)
-  # card = Card.new(:club, value, card_count)
-  # card = Card.new(:spade, value, card_count)
-  card_count += 1
-end
-binding.pry
 
 deck1 = Deck.new([])
 deck2 = Deck.new([])
 player1 = Player.new("Megan", deck1)
 player2 = Player.new("Aurora", deck2)
+turn = Turn.new(player1, player2)
+card_holder = []
+card_value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+card_value.each do |value|
+  card_count = 1
+  card_holder << Card.new(:heart, value, card_count + 1)
+  card_holder << Card.new(:diamond, value, card_count + 1)
+  card_holder << Card.new(:club, value, card_count + 1)
+  card_holder << Card.new(:spade, value, card_count + 1)
+  card_count += 1
+end
+player1.deck.cards << card_holder.shift(25)
+player2.deck.cards << card_holder
+
+
+
 
 
 
--- test/war_or_peace_runner_test.rb
+++ test/war_or_peace_runner_test.rb
@@ -8,13 +8,40 @@ require './lib/turn'
 class TurnTest < Minitest::Test
 
   def test_52_cards
+    card_holder = []
+    card_value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    card_value.each do |value|
+      card_count = 1
+      card_holder << Card.new(:heart, value, card_count + 1)
+      card_holder << Card.new(:diamond, value, card_count + 1)
+      card_holder << Card.new(:club, value, card_count + 1)
+      card_holder << Card.new(:spade, value, card_count + 1)
+      card_count += 1
+    end
+
+    assert_equal 52, card_holder.count
+  end
+
+  def test_deck_build
     deck1 = Deck.new([])
     deck2 = Deck.new([])
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
     card_holder = []
-    card_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-    card_suits = [:heart, :diamond, :club, :spade]
+    card_value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    card_value.each do |value|
+      card_count = 1
+      card_holder << Card.new(:heart, value, card_count + 1)
+      card_holder << Card.new(:diamond, value, card_count + 1)
+      card_holder << Card.new(:club, value, card_count + 1)
+      card_holder << Card.new(:spade, value, card_count + 1)
+      card_count += 1
+    end
+    player1.deck.cards << card_holder.shift(25)
+    player2.deck.cards << card_holder
 
-    assert_equal card_holder = []
-  end 
+    assert_equal player1.deck.cards.count, player2.deck.cards.count
+  end
+
+end
